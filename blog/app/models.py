4cfbe82ff6475ff6ad5202442de61949277541e6
Korolev Pavel <korolev.vrn@gmail.com>
2020-02-29 12:37:42 +0300

    Init

---
+++ blog/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField
+    status = models.CharField(max_length=10, choices=[('D', 'draft'), ('P', 'published')])
+    content = models.TextField()
+    updated = models.DateTimeField(default=timezone.now)
+    publication_date = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, null = True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name}'
